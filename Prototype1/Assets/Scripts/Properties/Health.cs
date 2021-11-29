6a9873988287902f35995c4c86962e76b876d38d
mihak1 <36701529+mihak1@users.noreply.github.com>
2021-11-29 16:09:41 +0100

    Add Health class

---
+++ Prototype1/Assets/Scripts/Properties/Health.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public double MaximumHitPoints = 100;
+
+    private double HitPoints;
+
+    void Awake()
+    {
+        HitPoints = MaximumHitPoints;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Damage(double damage)
+    {
+        HitPoints -= damage;
+
+        if (HitPoints <= 0)
+        {
+            // Die
+        }
+    }
+
+    public void Heal(double heal)
+    {
+        HitPoints += heal;
+
+        if (HitPoints > MaximumHitPoints)
+        {
+            HitPoints = MaximumHitPoints;
+        }
+    }
+}
