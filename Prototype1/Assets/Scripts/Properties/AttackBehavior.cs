a28802b2939f7bd303328b250ddc6fe7c9dba7d0
wardplaced <sebastian.wardana@student.tugraz.at>
2021-11-29 15:30:16 +0100

    this is how a property of an ingame object, like Attack could look like.
    I have kept in mind that some characters, epic or not may not carry certain values, like attacking, etc. so i have decided to seperate them, so they can be mixed and matched as you like, keeping Ekaterinas comments in mind and to have sensible balancing decisions for all attributes in place.

---
+++ Prototype1/Assets/Scripts/Properties/AttackBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehavior : MonoBehaviour
+{
+    public double Attack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // just to give it a base value if nothing has changed.
+        Attack = 40;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // load either script values from gameobject refferences through flags
+    }
+
+    public void generateAttack(double progression)
+    {
+
+    }
+}
