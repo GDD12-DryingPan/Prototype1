be1182fc43f9a9afcc3e32facbf3f4bfa4406718
mihak1 <36701529+mihak1@users.noreply.github.com>
2021-11-29 16:33:44 +0100

    Add Defence, Haste and Resistance classes

---
+++ Prototype1/Assets/Scripts/Properties/DefenceBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenceBehaviour : MonoBehaviour
+{
+    public double Defence = 100;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Prototype1/Assets/Scripts/Properties/ResistanceBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResistanceBehaviour : MonoBehaviour
+{
+    public double Resistance = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
