c2474aad80d01719cce2e9163f94c36e4beac74d
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-02 12:04:48 +0100

    all grid cells are numbered and carry their respective values inside of them
    if the project version gets pushed this time again, i hope at least that the gitignore catches it,

--- Prototype1/Assets/Scripts/Grid/GridMechanic.cs
+++ Prototype1/Assets/Scripts/Grid/GridMechanic.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class GridMechanic : MonoBehaviour
 {
     public GameObject gameObject;
+    public int fieldNumber;
+    public char fieldCharacter;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject = null;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject == null)
-        {
-            // do nothing
-        }
+
     }
 }
