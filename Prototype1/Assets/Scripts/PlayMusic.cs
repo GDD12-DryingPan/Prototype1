686f69e5ad99d1f315a50df31919d6b9289b582b
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-11 16:24:37 +0100

    i dont know why but it fixed it for me

--- Prototype1/Assets/Scripts/PlayMusic.cs
+++ Prototype1/Assets/Scripts/PlayMusic.cs
@@ -9,7 +9,7 @@ public class PlayMusic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        song.Play(0);
+        song.Play(4);
     }
 
     // Update is called once per frame
