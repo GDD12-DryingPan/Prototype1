f9f016f1242f0fc51937f43061d52f1f8330f498
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-11 13:19:44 +0100

    old man has been added
    again, this is not a prefab yet, it still needs lots of work done on it, so yeah, that is why.
    
    Same theme here though, there is a central script for collision.

---
+++ Prototype1/Assets/Scripts/Properties/OldMan.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldMan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // in case the old man is inside the attack box collider of collides with anything else
+    void OnTriggerEnter2D(Collider2D c)
+    {
+
+    }
+}
