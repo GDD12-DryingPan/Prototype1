6fb6eb5fcc63e5eb020dba437bebcb684630fc19
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-11 13:30:07 +0100

    OldMan is one of our characters
    So he will load in the prefab character and now we can access the contents of the prefab and set them in script, I hope!

--- Prototype1/Assets/Scripts/Properties/OldMan.cs
+++ Prototype1/Assets/Scripts/Properties/OldMan.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OldMan : MonoBehaviour
+public class OldMan : MonoBehaviour 
 {
+    public GameObject characterProperties;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(characterProperties, gameObject.transform);
     }
 
     // Update is called once per frame
