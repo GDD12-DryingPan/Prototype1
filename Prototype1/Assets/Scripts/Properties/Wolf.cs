0a0701b5b5c7b42ae1aedfe285a7c28a6a354631
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-11 13:05:36 +0100

    Added Wolf script and WolfAttackMove script
    and offcourse also the ontriggerenter2d for all the necessary interaction, and triggering.
    
    Remember, the enemy and characters are only allowed to attack once after moving, they have to move again, if they can in order to attack, if you cant attack a character than attack does not happen.
    
    Just keep that in mind when thinking how are we going to figure out turn based combat.

---
+++ Prototype1/Assets/Scripts/Properties/Wolf.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : MonoBehaviour
+
+    // we can load in the behavior scripts here so the wolf script knows what atk and health it has. 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // this is for when the wolf gets attacked
+    void OnTriggerEnter2D(Collider2D c)
+    {
+
+    }
+}
---
+++ Prototype1/Assets/Scripts/Properties/WolfAttackMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAttackMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // this is for the colider box in front of the wolf
+    void OnTriggerEnter2D(Collider2D c)
+    {
+
+    }
+}
