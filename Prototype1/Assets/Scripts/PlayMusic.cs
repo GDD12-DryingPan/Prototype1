acf19da95dc3cbca3a06caff5c3fa298ed2ecb8b
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-11 16:14:28 +0100

    the game scene how has a sound track and it plays
    wonderful work miha

--- Prototype1/Assets/Scripts/PlayMusic.cs
+++ Prototype1/Assets/Scripts/PlayMusic.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayMusic : MonoBehaviour
 {
+
+    public AudioSource song;
     // Start is called before the first frame update
     void Start()
     {
-        
+        song.Play(0);
     }
 
     // Update is called once per frame
