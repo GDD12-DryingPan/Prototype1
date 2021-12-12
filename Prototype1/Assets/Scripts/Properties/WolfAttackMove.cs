23eab3a364ae5be15bc83338488cc810cf05f72b
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-12 14:33:54 +0100

    we can officially read out the data the designers will put into the unity editor for our game to use
    this will allow us to have attacks, defenses etc. and actually use the values that are set by adding the script and putting the value into the editor!

--- Prototype1/Assets/Scripts/Properties/WolfAttackMove.cs
+++ Prototype1/Assets/Scripts/Properties/WolfAttackMove.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class WolfAttackMove : MonoBehaviour
 {
+    private AttackBehaviour attackValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // this is the way we will access all the attk, def, res, and character values! it works!!! 
+        attackValue = transform.parent.gameObject.GetComponent<AttackBehaviour>();
+        Debug.Log("Attack of Wolf: "+attackValue.Attack);
     }
 
     // Update is called once per frame
