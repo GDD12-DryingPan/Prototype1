3cfc294aa5b81c251a31914c9ccb28758c5f1e92
wardplaced <sebastian.wardana@student.tugraz.at>
2021-12-17 10:49:16 +0100

    it just doesnt register
    this is bad!

--- Prototype1/Assets/Scripts/MouseControlCharacter.cs
+++ Prototype1/Assets/Scripts/MouseControlCharacter.cs
@@ -4,57 +4,25 @@ using UnityEngine;
 
 public class MouseControlCharacter : MonoBehaviour
 {
-    GameObject g1;
-    GameObject g2;
-    bool mouseClickedOnce;
-    bool mouseClickedTwice;
-    bool mouseHasContent;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("entered Start method at MouseControlCharacter");
-        mouseClickedOnce = false;
-        mouseClickedTwice = false;
-        mouseHasContent = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // works
-        if (mouseClickedOnce)
-            Debug.Log("Registered MouseClick by boolean mouseClickedOnce");
-        if (mouseClickedOnce && mouseHasContent)
-        {
-            // Mouse has content doesnt work!
-            Debug.Log("Mouse Has Content");
-            mouseClickedTwice = true;
-        }
-        mouseClickedOnce = Input.GetMouseButtonDown(0);
+
     }
 
-    void OnTriggerEnter2D(Collider2D c)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (c.gameObject.tag == "Grid" && mouseClickedTwice)
-        {
-            Debug.Log("Clicked Grid after clicked on Character");
-            c.gameObject.GetComponent<GridMechanic>().gameObject1 = g1;
-            mouseClickedOnce = false;
-            mouseClickedTwice = false;
-            mouseHasContent = false;
-        }
-        if (c.gameObject.tag == "OldMan" && mouseClickedOnce)
-        {
-            Debug.Log("You selected the old man.");
-            g1 = c.gameObject;
-            mouseHasContent = true;
-        }
-        // later
-        if (c.gameObject.tag == "Wolf" && mouseClickedOnce)
-        {
-            g1 = c.gameObject;
-        }
+        Debug.Log("registered mouse hover");
 
-        mouseClickedOnce = Input.GetMouseButtonUp(0);
+        if (collider.gameObject.tag == "OldMan")
+        {
+
+        }
     }
 }
