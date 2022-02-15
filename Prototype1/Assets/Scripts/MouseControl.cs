c760c1ff8622ae6a82df4b470123e338c71cba21
wardplaced <sebastian.wardana@student.tugraz.at>
2022-02-15 16:52:23 +0100

    added first steps in making limited movement possible for old man
    first of all, sorry for not making lots of small commits.
    
    i am just making it possible to set old mans position on the screen automatically through collision, setting that value to the old man script, accessing it in the mouse control script and i have added an empty, possibly private method that we will use to express the rules for movement.
    
    Core question here is, how far is the old man able to move inside of the grid.
    
    If we can determin that by comparing old mans current position on the grid, which is familiar and something we can messure, with the grid that has been clicked on, we can clearly set rules for all future characters of the squad that the player can control.
    
    so a huge milestone has already been figured out, just by knowing, which grid the player has clicked on and on which grid a selected character has been standing on.
    
    the rest is just comparing characters and integers, we just have not clearly stated yet how the old man is able to move.

--- Prototype1/Assets/Scripts/MouseControl.cs
+++ Prototype1/Assets/Scripts/MouseControl.cs
@@ -29,13 +29,36 @@ public class MouseControl : MonoBehaviour
                 {
                     OldManSelected = true;
                     OldMan = raycastHit.transform;
+                    OldMan oldman = raycastHit.transform.gameObject.GetComponent<>();
                 }
                 else if (raycastHit.transform.tag == "Grid" && OldManSelected)
                 {
+                    // deselect old man
                     OldManSelected = false;
+                    // get fieldNumber and fieldCharacter from scriptobject, but first get the clicked on grids script
+                    GridMechanic grid = raycastHit.transform.gameObject.GetComponent<GridMechanic>();
+
+                    // we want to check where the old man or any character is going before letting them go there
+                    // for now we will output a debug log so we know the input has been registered and taken care off
+
+                    int number = grid.fieldNumber;
+                    char character = grid.fieldCharacter;
+
+                    // we will compare how the old mans grid number and character correlate to the clicked on grid and if 
+                    // the old man is allowed to move there or not
+                    // but not today.
+
+                    // NOTE: Open scripts OldMan and GridMechanic to work on functionality 
+
                     OldMan.position = raycastHit.transform.position;
                 }
             }
         }
+    }
+
+    // maybe every character has their own method to keep update small
+    void OldManMovementRules()
+    {
+        
     }
 }
