3e16c92cba29172f7972aa44555e2d4ea15cc557
mihak1 <36701529+mihak1@users.noreply.github.com>
2022-02-16 14:43:40 +0100

    Add deck management

--- Prototype1/Assets/Scripts/Cards/Cards.cs
+++ Prototype1/Assets/Scripts/Cards/Cards.cs
@@ -2,23 +2,41 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cards : MonoBehaviour
 {
+    public List<GameObject> CardButtons;
+    public List<Card> InitialDeck;
+
+    private IList<Card> Deck = new List<Card>();
+
     private IList<Card> DrawPile = new List<Card>();
     private IList<Card> Hand = new List<Card>();
     private IList<Card> DiscardPile = new List<Card>();
 
-    // Add cards from deck to the draw pile and shuffle it
-    public void Initialize(IList<Card> cards)
+    void Start()
     {
-        foreach (Card card in cards)
+        if (Deck.Count() == 0)
+        {
+            foreach (Card card in InitialDeck)
+            {
+                Deck.Add(card);
+            }
+        }
+
+        // Add cards to the draw pile
+        foreach (Card card in Deck)
         {
             DrawPile.Add(card);
         }
 
+        // Shuffle the deck
         var random = new System.Random();
         DrawPile = DrawPile.OrderBy(x => random.Next()).ToList();
+
+        // Draw the cards
+        DrawCards(3);
     }
 
     // Draw cards from draw pile
@@ -26,10 +44,19 @@ public class Cards : MonoBehaviour
     {
         int cardsLeft = numberOfCards;
 
+        // Draw cards from draw pile
+        IList<int> cardIndicesToRemove = new List<int>();
         for (int i = 0; i < System.Math.Min(DrawPile.Count(), numberOfCards); i++)
         {
             Hand.Add(DrawPile.ElementAt(i));
-            DrawPile.RemoveAt(i);
+            cardIndicesToRemove.Add(i);
+
+            cardsLeft--;
+        }
+
+        foreach (int i in cardIndicesToRemove)
+        {
+            DrawPile.RemoveAt(0);
         }
 
         // If we still have cards to draw
@@ -39,52 +66,59 @@ public class Cards : MonoBehaviour
             for (int i = 0; i < DiscardPile.Count(); i++)
             {
                 DrawPile.Add(DiscardPile.ElementAt(i));
-                DiscardPile.RemoveAt(i);
             }
+            DiscardPile.Clear();
 
             var random = new System.Random();
             DrawPile = DrawPile.OrderBy(x => random.Next()).ToList();
         }
 
         // Draw the remaining number of cards
+        cardIndicesToRemove.Clear();
         for (int i = 0; i < System.Math.Min(DrawPile.Count(), cardsLeft); i++)
         {
             Hand.Add(DrawPile.ElementAt(i));
-            DrawPile.RemoveAt(i);
+            cardIndicesToRemove.Add(i);
+        }
+
+        foreach (int i in cardIndicesToRemove)
+        {
+            DrawPile.RemoveAt(0);
+        }
+
+        // Show cards in hand
+        for (int i = 0; i < Hand.Count(); i++)
+        {
+            Button cardPrefabButton = Hand.ElementAt(i).gameObject.GetComponent<Button>();
+
+            Button cardButton = CardButtons.ElementAt(i).GetComponent<Button>();
+            cardButton.image.sprite = cardPrefabButton.image.sprite;
         }
     }
 
     // Play a card in hand
-    public void PlayCard(int i, GameObject target)
+    public void PlayCard(int i)
     {
-        // TODO: Add card effect
-        if (this.gameObject.GetComponent<AttackScript>() != null)
-        {
-            this.gameObject.GetComponent<AttackScript>().attackAction(target);
-        }
-        if (this.gameObject.GetComponent<HealScript>() != null)
-        {
-            this.gameObject.GetComponent<HealScript>().healAction(target);
-        }
-        if (this.gameObject.GetComponent<ShieldScript>() != null)
-        {
-            this.gameObject.GetComponent<ShieldScript>().shieldAction(target);
-        }
+        // TODO: Add card effect (attack, protect, heal ...)
 
         DiscardPile.Add(Hand.ElementAt(i));
         Hand.RemoveAt(i);
+
+        // TODO: Incorporate into turn-based combat
+        // At the moment we discard the hand and end turn as soon as any card is played
+        DiscardHand();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    // Discard hand at the end of the turn
+    public void DiscardHand()
     {
-        
-    }
+        foreach (Card card in Hand)
+        {
+            DiscardPile.Add(card);
+        }
+        Hand.Clear();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Draw cards after discarding
+        DrawCards(3);
     }
 }
